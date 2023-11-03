84dc768d492674c7d8db20621a10d7da0b0c7503
Renata <opel5323@gmail.com>
2023-11-03 23:48:16 +0300

    135.cpp

---
+++ 135.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int main()
+{setlocale(0, ""); using namespace std;
+int r{}, u{}, i{}, o{}; 
+while (true) {cout << "Введите число: "; cin >> r;
+++o;u += r;i = u / o;char yn;	
+cout << "Завершить y, нет n: "; cin >> yn;
+if (yn == 'y' or yn == 'Y') break;}
+cout << "Сумма: " << u << endl; cout << "Среднее арифметическое: " << i;
+int _; cin >> _;}  
+
+		
